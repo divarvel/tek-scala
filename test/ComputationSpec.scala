0f8bfd36dc8fb9f6495c30010b9c11228303d28d
Clement Delafargue <clement@delafargue.name>
2016-03-11 11:29:41 +0100

    Test base

---
+++ test/ComputationSpec.scala
@@ -0,0 +1,17 @@
+import org.scalatestplus.play._
+import play.api.test._
+import play.api.test.Helpers._
+
+/**
+ * Add your spec here.
+ * You can mock out a whole application including requests, plugins etc.
+ * For more information, consult the wiki.
+ */
+class ComputationSpec extends PlaySpec  {
+  "minPrice" should {
+    "return the smallest value" in {
+      // ToDo write your tests
+      false mustBe true
+    }
+  }
+}
